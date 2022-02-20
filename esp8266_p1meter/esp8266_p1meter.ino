431c7cbad3cb9dcfc3005cebb97ec29dd5d8aeff
René Arts <renearts@gmail.com>
2022-02-20 19:58:52 +0100

    Change sent values over mqtt to float

--- esp8266_p1meter/esp8266_p1meter.ino
+++ esp8266_p1meter/esp8266_p1meter.ino
@@ -113,7 +113,7 @@ bool mqtt_reconnect()
     return true;
 }
 
-void send_metric(String name, long metric)
+void send_metric(String name, float metric)
 {
     Serial.print(F("Sending metric to broker: "));
     Serial.print(name);
@@ -121,7 +121,8 @@ void send_metric(String name, long metric)
     Serial.println(metric);
 
     char output[10];
-    ltoa(metric, output, sizeof(output));
+//    ltoa(metric, output, sizeof(output)); // I'd rather have float outputs in kWh
+    dtostrf(metric,1,3,output);
 
     String topic = String(MQTT_ROOT_TOPIC) + "/" + name;
     send_mqtt_message(topic.c_str(), output);
@@ -203,7 +204,7 @@ int FindCharInArrayRev(char array[], char c, int len)
     return -1;
 }
 
-long getValue(char *buffer, int maxlen, char startchar, char endchar)
+float getValue(char *buffer, int maxlen, char startchar, char endchar)
 {
     int s = FindCharInArrayRev(buffer, startchar, maxlen - 2);
     int l = FindCharInArrayRev(buffer, endchar, maxlen - 2) - s - 1;
@@ -217,7 +218,7 @@ long getValue(char *buffer, int maxlen, char startchar, char endchar)
         {
             if (isNumber(res, l))
                 // * Lazy convert float to long
-                return (1000 * atof(res));
+                return (atof(res)); // keep kWh format
         }
         else if (endchar == ')')
         {
@@ -329,296 +330,7 @@ bool decode_telegram(int len)
         L3_INSTANT_POWER_USAGE = getValue(telegram, len, '(', '*');
     }
 
-    // 1-0:31.7.0(002*A)
-    // 1-0:31.7.0 = Instantane stroom Elektriciteit L1
-    if (strncmp(telegram, "1-0:31.7.0", strlen("1-0:31.7.0")) == 0)
-    {
-        L1_INSTANT_POWER_CURRENT = getValue(telegram, len, '(', '*');
-    }
-    // 1-0:51.7.0(002*A)
-    // 1-0:51.7.0 = Instantane stroom Elektriciteit L2
-    if (strncmp(telegram, "1-0:51.7.0", strlen("1-0:51.7.0")) == 0)
-    {
-        L2_INSTANT_POWER_CURRENT = getValue(telegram, len, '(', '*');
-    }
-    // 1-0:71.7.0(002*A)
-    // 1-0:71.7.0 = Instantane stroom Elektriciteit L3
-    if (strncmp(telegram, "1-0:71.7.0", strlen("1-0:71.7.0")) == 0)
-    {
-        L3_INSTANT_POWER_CURRENT = getValue(telegram, len, '(', '*');
-    }
-
-    // 1-0:32.7.0(232.0*V)
-    // 1-0:32.7.0 = Voltage L1
-    if (strncmp(telegram, "1-0:32.7.0", strlen("1-0:32.7.0")) == 0)
-    {
-        L1_VOLTAGE = getValue(telegram, len, '(', '*');
-    }
-    // 1-0:52.7.0(232.0*V)
-    // 1-0:52.7.0 = Voltage L2
-    if (strncmp(telegram, "1-0:52.7.0", strlen("1-0:52.7.0")) == 0)
-    {
-        L2_VOLTAGE = getValue(telegram, len, '(', '*');
-    }   
-    // 1-0:72.7.0(232.0*V)
-    // 1-0:72.7.0 = Voltage L3
-    if (strncmp(telegram, "1-0:72.7.0", strlen("1-0:72.7.0")) == 0)
-    {
-        L3_VOLTAGE = getValue(telegram, len, '(', '*');
-    }
-
-    // 0-1:24.2.1(150531200000S)(00811.923*m3)
-    // 0-1:24.2.1 = Gas (DSMR v4.0) on Kaifa MA105 meter
-    if (strncmp(telegram, "0-1:24.2.1", strlen("0-1:24.2.1")) == 0)
-    {
-        GAS_METER_M3 = getValue(telegram, len, '(', '*');
-    }
-
-    // 0-0:96.14.0(0001)
-    // 0-0:96.14.0 = Actual Tarif
-    if (strncmp(telegram, "0-0:96.14.0", strlen("0-0:96.14.0")) == 0)
-    {
-        ACTUAL_TARIF = getValue(telegram, len, '(', ')');
-    }
-
-    // 0-0:96.7.21(00003)
-    // 0-0:96.7.21 = Aantal onderbrekingen Elektriciteit
-    if (strncmp(telegram, "0-0:96.7.21", strlen("0-0:96.7.21")) == 0)
-    {
-        SHORT_POWER_OUTAGES = getValue(telegram, len, '(', ')');
-    }
-
-    // 0-0:96.7.9(00001)
-    // 0-0:96.7.9 = Aantal lange onderbrekingen Elektriciteit
-    if (strncmp(telegram, "0-0:96.7.9", strlen("0-0:96.7.9")) == 0)
-    {
-        LONG_POWER_OUTAGES = getValue(telegram, len, '(', ')');
-    }
-
-    // 1-0:32.32.0(00000)
-    // 1-0:32.32.0 = Aantal korte spanningsdalingen Elektriciteit in fase 1
-    if (strncmp(telegram, "1-0:32.32.0", strlen("1-0:32.32.0")) == 0)
-    {
-        SHORT_POWER_DROPS = getValue(telegram, len, '(', ')');
-    }
-
-    // 1-0:32.36.0(00000)
-    // 1-0:32.36.0 = Aantal korte spanningsstijgingen Elektriciteit in fase 1
-    if (strncmp(telegram, "1-0:32.36.0", strlen("1-0:32.36.0")) == 0)
-    {
-        SHORT_POWER_PEAKS = getValue(telegram, len, '(', ')');
-    }
-
-    return validCRCFound;
-}
-
-void read_p1_hardwareserial()
-{
-    if (Serial.available())
-    {
-        memset(telegram, 0, sizeof(telegram));
-
-        while (Serial.available())
-        {
-            ESP.wdtDisable();
-            int len = Serial.readBytesUntil('\n', telegram, P1_MAXLINELENGTH);
-            ESP.wdtEnable(1);
-
-            processLine(len);
-        }
-    }
-}
-
-void processLine(int len) {
-    telegram[len] = '\n';
-    telegram[len + 1] = 0;
-    yield();
-
-    bool result = decode_telegram(len + 1);
-    if (result) {
-        send_data_to_broker();
-        LAST_UPDATE_SENT = millis();
-    }
-}
-
-// **********************************
-// * EEPROM helpers                 *
-// **********************************
-
-String read_eeprom(int offset, int len)
-{
-    Serial.print(F("read_eeprom()"));
-
-    String res = "";
-    for (int i = 0; i < len; ++i)
-    {
-        res += char(EEPROM.read(i + offset));
-    }
-    return res;
-}
-
-void write_eeprom(int offset, int len, String value)
-{
-    Serial.println(F("write_eeprom()"));
-    for (int i = 0; i < len; ++i)
-    {
-        if ((unsigned)i < value.length())
-        {
-            EEPROM.write(i + offset, value[i]);
-        }
-        else
-        {
-            EEPROM.write(i + offset, 0);
-        }
-    }
-}
-
-// ******************************************
-// * Callback for saving WIFI config        *
-// ******************************************
-
-bool shouldSaveConfig = false;
-
-// * Callback notifying us of the need to save config
-void save_wifi_config_callback ()
-{
-    Serial.println(F("Should save config"));
-    shouldSaveConfig = true;
-}
-
-// **********************************
-// * Setup OTA                      *
-// **********************************
-
-void setup_ota()
-{
-    Serial.println(F("Arduino OTA activated."));
-
-    // * Port defaults to 8266
-    ArduinoOTA.setPort(8266);
-
-    // * Set hostname for OTA
-    ArduinoOTA.setHostname(HOSTNAME);
-    ArduinoOTA.setPassword(OTA_PASSWORD);
-
-    ArduinoOTA.onStart([]()
-    {
-        Serial.println(F("Arduino OTA: Start"));
-    });
-
-    ArduinoOTA.onEnd([]()
-    {
-        Serial.println(F("Arduino OTA: End (Running reboot)"));
-    });
-
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
-    {
-        Serial.printf("Arduino OTA Progress: %u%%\r", (progress / (total / 100)));
-    });
-
-    ArduinoOTA.onError([](ota_error_t error)
-    {
-        Serial.printf("Arduino OTA Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR)
-            Serial.println(F("Arduino OTA: Auth Failed"));
-        else if (error == OTA_BEGIN_ERROR)
-            Serial.println(F("Arduino OTA: Begin Failed"));
-        else if (error == OTA_CONNECT_ERROR)
-            Serial.println(F("Arduino OTA: Connect Failed"));
-        else if (error == OTA_RECEIVE_ERROR)
-            Serial.println(F("Arduino OTA: Receive Failed"));
-        else if (error == OTA_END_ERROR)
-            Serial.println(F("Arduino OTA: End Failed"));
-    });
-
-    ArduinoOTA.begin();
-    Serial.println(F("Arduino OTA finished"));
-}
-
-// **********************************
-// * Setup MDNS discovery service   *
-// **********************************
-
-void setup_mdns()
-{
-    Serial.println(F("Starting MDNS responder service"));
-
-    bool mdns_result = MDNS.begin(HOSTNAME);
-    if (mdns_result)
-    {
-        MDNS.addService("http", "tcp", 80);
-    }
-}
-
-// **********************************
-// * Setup Main                     *
-// **********************************
-
-void setup()
-{
-    // * Configure EEPROM
-    EEPROM.begin(512);
-
-    // Setup a hw serial connection for communication with the P1 meter and logging (not using inversion)
-    Serial.begin(BAUD_RATE, SERIAL_8N1, SERIAL_FULL);
-    Serial.println("");
-    Serial.println("Swapping UART0 RX to inverted");
-    Serial.flush();
-
-    // Invert the RX serialport by setting a register value, this way the TX might continue normally allowing the serial monitor to read println's
-    USC0(UART0) = USC0(UART0) | BIT(UCRXI);
-    Serial.println("Serial port is ready to recieve.");
-
-    // * Set led pin as output
-    pinMode(LED_BUILTIN, OUTPUT);
-
-    // * Start ticker with 0.5 because we start in AP mode and try to connect
-    ticker.attach(0.6, tick);
-
-    // * Get MQTT Server settings
-    String settings_available = read_eeprom(134, 1);
-
-    if (settings_available == "1")
-    {
-        read_eeprom(0, 64).toCharArray(MQTT_HOST, 64);   // * 0-63
-        read_eeprom(64, 6).toCharArray(MQTT_PORT, 6);    // * 64-69
-        read_eeprom(70, 32).toCharArray(MQTT_USER, 32);  // * 70-101
-        read_eeprom(102, 32).toCharArray(MQTT_PASS, 32); // * 102-133
-    }
-
-    WiFiManagerParameter CUSTOM_MQTT_HOST("host", "MQTT hostname", MQTT_HOST, 64);
-    WiFiManagerParameter CUSTOM_MQTT_PORT("port", "MQTT port",     MQTT_PORT, 6);
-    WiFiManagerParameter CUSTOM_MQTT_USER("user", "MQTT user",     MQTT_USER, 32);
-    WiFiManagerParameter CUSTOM_MQTT_PASS("pass", "MQTT pass",     MQTT_PASS, 32);
-
-    // * WiFiManager local initialization. Once its business is done, there is no need to keep it around
-    WiFiManager wifiManager;
-
-    // * Reset settings - uncomment for testing
-    // wifiManager.resetSettings();
-
-    // * Set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
-    wifiManager.setAPCallback(configModeCallback);
-
-    // * Set timeout
-    wifiManager.setConfigPortalTimeout(WIFI_TIMEOUT);
-
-    // * Set save config callback
-    wifiManager.setSaveConfigCallback(save_wifi_config_callback);
-
-    // * Add all your parameters here
-    wifiManager.addParameter(&CUSTOM_MQTT_HOST);
-    wifiManager.addParameter(&CUSTOM_MQTT_PORT);
-    wifiManager.addParameter(&CUSTOM_MQTT_USER);
-    wifiManager.addParameter(&CUSTOM_MQTT_PASS);
-
-    // * Fetches SSID and pass and tries to connect
-    // * Reset when no connection after 10 seconds
-    if (!wifiManager.autoConnect())
-    {
-        Serial.println(F("Failed to connect to WIFI and hit timeout"));
-
-        // * Reset and try again, or maybe put it to deep sleep
+    // 1-0:31.7.0(002*A)m fd,
         ESP.reset();
         delay(WIFI_TIMEOUT);
     }
@@ -673,12 +385,11 @@ void loop()
 
     if (!mqtt_client.connected())
     {
-        if (now - LAST_RECONNECT_ATTEMPT > 5000)
+        if (now - LAST_RECONNECT_ATTEMPT > 5000)cdf
         {
-            LAST_RECONNECT_ATTEMPT = now;
-
-            if (mqtt_reconnect())
-            {
+            LAST_RECONNECT_ATTEMPT = now;longw
+            if (mqtt_reconnect())long 3g 
+            { long 
                 LAST_RECONNECT_ATTEMPT = 0;
             }
         }
@@ -691,4 +402,4 @@ void loop()
     if (now - LAST_UPDATE_SENT > UPDATE_INTERVAL) {
         read_p1_hardwareserial();
     }
-}
+}     x
